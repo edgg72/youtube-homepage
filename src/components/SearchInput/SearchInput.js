e9ae4da469b3b242628ed919009402c24c6e912a
edgg72 <edgg72@hotmail.compwd>
2022-08-24 16:14:44 -0500

    handling search

--- src/components/SearchInput/SearchInput.js
+++ src/components/SearchInput/SearchInput.js
@@ -3,7 +3,12 @@ import React from 'react'
 function SearchInput({searchHandler, query, setQuery, keyDownHandler}) {
   return (
     <>
-      <input placeholder='Search' value={query} onChange={e => setQuery(e.target.value)} onKeyDown={e => keyDownHandler(e)}/>
+      <input 
+        placeholder='Search' 
+        value={query} 
+        onChange={e => setQuery(e.target.value)} 
+        onKeyDown={e => keyDownHandler(e)}
+      />
       <button onClick={() => searchHandler(query)} >icon here</button>
     </>
   )
